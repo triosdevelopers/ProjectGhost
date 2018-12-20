29f249e15f974a3ec00a10e961a5423938f32ffb
Andrew Prior <Andrew.Prior@triosstudent.com>
2018-12-20 11:16:00 -0500

    added basic navigational stuff on admin page.

--- ProjectGhost/wwwroot/js/site.js
+++ ProjectGhost/wwwroot/js/site.js
@@ -2,6 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// for login and account creation button thing
 $(document).ready(function(){
   $('#login-trigger').click(function(){
     $(this).next('#login-content').slideToggle();
@@ -11,3 +13,25 @@ $(document).ready(function(){
       else $(this).find('span').html('&#x25BC;')
     })
 });
+
+// for tabs
+function openOptions(evt, optionChoice) {
+  // Declare all variables
+  var i, options, tabOptions;
+
+  // Get all elements with class="tabcontent" and hide them
+  options = document.getElementsByClassName("options");
+  for (i = 0; i < options.length; i++) {
+    options[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tabOptions = document.getElementsByClassName("tabOptions");
+  for (i = 0; i < tabOptions.length; i++) {
+    tabOptions[i].className = tabOptions[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(optionChoice).style.display = "block";
+  evt.currentTarget.optionChoice += " active";
+}
