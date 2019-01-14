7a2b8de6b2dc147748fc086dfaed8139cba29692
Andrew Prior <Andrew.Prior@triosstudent.com>
2019-01-14 11:36:49 -0500

    added range sliders onto admin.

--- ProjectGhost/wwwroot/js/site.js
+++ ProjectGhost/wwwroot/js/site.js
@@ -35,3 +35,14 @@ function openOptions(evt, optionChoice) {
   document.getElementById(optionChoice).style.display = "block";
   evt.currentTarget.optionChoice += " active";
 }
+
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+	output.innerHTML = this.value;
+}
