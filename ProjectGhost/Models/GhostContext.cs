dd97041f6daa558c2af02ec1cb0d53b1090b7f74
Devon Kassian <Devon.Kassian@triosstudent.com>
2019-01-22 11:47:27 -0500

    with database and migration

---
+++ ProjectGhost/Models/GhostContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ghost_Db.Models
+{
+    public class GhostContext : DbContext
+    {
+        public GhostContext(DbContextOptions<GhostContext> options)
+             : base(options) { }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Capture> Capture { get; set; }
+        public DbSet<Ghost> Ghost { get; set; }
+        public DbSet<GhostType> GhostType { get; set; }
+        public DbSet<CameraSchedule> CameraSchedule { get; set; }
+        public DbSet<GhostProtocols> GhostProtocol { get; set; }
+    
+        
+    }
+
+}
--- ProjectGhost/Startup.cs
+++ ProjectGhost/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ghost_Db.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,7 +26,10 @@ namespace ProjectGhost
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<CookiePolicyOptions>(options =>
+            services.AddDbContext<GhostContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("GhostConn")));
+
+            services.Configure<CookiePolicyOptions>(options =>
 			{
 				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
 				options.CheckConsentNeeded = context => true;
