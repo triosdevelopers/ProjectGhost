eda46367d9b899accb22c847d0c577b0a47b1edb
Andrew Prior <Andrew.Prior@triosstudent.com>
2019-02-01 09:06:19 -0500

    Created ghost animation on home page.

--- ProjectGhost/MyTcpListener.cs
+++ ProjectGhost/MyTcpListener.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -85,4 +85,4 @@ namespace ProjectGhost
             Console.Read();
         }
     }
-}
+}*/
